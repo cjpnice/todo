19387c49e0f17ad1f3b454e9ea5e537bdc65c69b
CHEN <cjpnice@qq.com>
2022-04-29 14:57:44 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     hotOnly: false,
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:8082/',
+          target: 'http://127.0.0.1:8081/',
           ws: true,
           changeOrigin: true
           
